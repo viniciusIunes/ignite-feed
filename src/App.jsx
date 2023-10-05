274a7ccd25e5efbbdd1345ef4f7545d857a8bb9e
vinicius <vinicius1054@gmail.com>
2023-10-05 15:46:26 -0300

    props manipulation

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,48 @@ import { Post } from "./components/Post";
 import styles from './App.module.css'
 import './global.css'
 
+/*
+  author : { avatarUrl: '', name: '', role: ''}
+  publishedAt: Date
+  content : String,
+*/
+
+const posts = [
+  {
+    id: 1,
+    author:  {
+      avatarUrl: 'https://github.com/viniciusiunes.png',
+      name: 'Vinícius Moreira',
+      role: 'Developer web'
+    },
+
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'}
+    ],
+
+    publishedAt: new Date('2023-10-03 20:00:00')
+  },
+
+    {
+    id: 2,
+    author:  {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Developer'
+    },
+
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'}
+    ],
+
+    publishedAt: new Date('2023-10-10 20:00:00')
+  }
+]
+
 export function App() {
  return (
   <div>
@@ -15,9 +57,17 @@ export function App() {
 
       <Sidebar />
       
+        {/*o map tem retorno, ele devolve algo, diferente do foreach*/ }
       <main>
-        <Post />
-        <Post />
+        {posts.map(post => {
+          return( 
+            <Post
+              author = {post.author}
+              content = {post.content}
+              publishedAt = {post.publishedAt}          
+            />
+          )
+        })}
       </main>
     </div>
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,44 +1,78 @@
-import { Avatar } from './Avatar'
-import { Comment } from './Comment'
+import { useState } from 'react'
+import { format, formatDistanceToNow } from 'date-fns'
+
+import ptBR from 'date-fns/locale/pt-BR'
 import styles from './Post.module.css'
 
-export function Post(){
+import { Avatar } from './Avatar'
+import { Comment } from './Comment'
+
+export function Post({ author, publishedAt, content}){
+  const [comments, setComments] = useState([
+    'Post muito bacana, hein!?'
+  ])
+  const [newCommentText, setNewCommentText] = useState('')
+
+
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'as' HH:mm'h'", {
+    locale : ptBR,
+  })
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+
+  })
+
+
+  
+  function handleCreateNewComment() {
+    event.preventDefault() // serve para impedir o comportamento default do html. 
+    setComments([...comments, newCommentText])  // imutabilidade     
+    setNewCommentText('')
+  }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value)
+  }
+
+
   return (
     <article className={styles.post}>
 
       <header className={styles.header}>
         <div className={styles.author}>
-          <Avatar src="https://github.com/viniciusiunes.png"  />
+          <Avatar src={author.avatarUrl}  />          
           <div className={styles.authorInfo}>
-            <strong>Vinícius Moreira</strong>
-            <span>Web developer</span>
+            <strong>{author.name}</strong>
+            
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time  title='11 de maio as 8h' dataTime='2022'>
-          publicado em 1h
+        <time  title={publishedDateFormatted} dataTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
         </time>
       </header> 
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
+        {content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p>{line.content}</p>
 
-        <p> Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-        <p>👉<a href="#"> jane.design/doctorcare</a></p>
-
-        <p>
-          <a href="#"> #novoprojeto</a>
-          <a href="#"> #nlw</a>
-          <a href="#"> #rocketseat</a>
-        </p>
+          } else if (line.type === 'link') {
+            return  <p><a href="#">{line.content}</a></p>
+          }
+        })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea
-          placeholder='Deixe um comentário'        
+          name='comment'
+          placeholder='Deixe um comentário'    
+          value={newCommentText}    
+          onChange={handleNewCommentChange}
         />
 
         <footer>
@@ -47,9 +81,9 @@ export function Post(){
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return <Comment content={comment} />
+        })}
       </div>
 
     </article>
